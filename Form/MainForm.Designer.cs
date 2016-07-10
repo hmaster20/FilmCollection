5d14ce834b484b1871a194b1f58cbac4517191c9
hmaster20 <hmaster20@gmail.com>
2016-07-10 16:10:20 +0300

    Редизайн

---
+++ Form/MainForm.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace FilmCollection
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.browserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.contextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.mainMenu = new System.Windows.Forms.MenuStrip();
+            this.MenuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuScan = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuLoad = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuTree = new System.Windows.Forms.ToolStripMenuItem();
+            this.treeFolder = new System.Windows.Forms.TreeView();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.tabControl2 = new System.Windows.Forms.TabControl();
+            this.tabFilm = new System.Windows.Forms.TabPage();
+            this.tabWriter = new System.Windows.Forms.TabPage();
+            this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.mainMenu.SuspendLayout();
+            this.tabControl2.SuspendLayout();
+            this.tabFilm.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(417, 411);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "Name";
+            this.Column1.HeaderText = "Название";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "Year";
+            this.Column2.HeaderText = "Год";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.DataPropertyName = "Type";
+            this.Column3.HeaderText = "Тип";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.DataPropertyName = "Path";
+            this.Column4.HeaderText = "Путь";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // browserDialog
+            // 
+            this.browserDialog.ShowNewFolderButton = false;
+            // 
+            // contextMenu
+            // 
+            this.contextMenu.Name = "contextMenuStrip1";
+            this.contextMenu.Size = new System.Drawing.Size(61, 4);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 467);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(942, 22);
+            this.statusStrip1.TabIndex = 18;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // mainMenu
+            // 
+            this.mainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuFile,
+            this.справкаToolStripMenuItem});
+            this.mainMenu.Location = new System.Drawing.Point(0, 0);
+            this.mainMenu.Name = "mainMenu";
+            this.mainMenu.Size = new System.Drawing.Size(942, 24);
+            this.mainMenu.TabIndex = 21;
+            this.mainMenu.Text = "menuStrip1";
+            // 
+            // MenuFile
+            // 
+            this.MenuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuScan,
+            this.MenuLoad,
+            this.MenuTree});
+            this.MenuFile.Name = "MenuFile";
+            this.MenuFile.Size = new System.Drawing.Size(48, 20);
+            this.MenuFile.Text = "Файл";
+            // 
+            // MenuScan
+            // 
+            this.MenuScan.Name = "MenuScan";
+            this.MenuScan.Size = new System.Drawing.Size(180, 22);
+            this.MenuScan.Text = "Сканировать папку";
+            this.MenuScan.Click += new System.EventHandler(this.btnScanDir_Click);
+            // 
+            // MenuLoad
+            // 
+            this.MenuLoad.Name = "MenuLoad";
+            this.MenuLoad.Size = new System.Drawing.Size(180, 22);
+            this.MenuLoad.Text = "Загрузить";
+            this.MenuLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // MenuTree
+            // 
+            this.MenuTree.Name = "MenuTree";
+            this.MenuTree.Size = new System.Drawing.Size(180, 22);
+            this.MenuTree.Text = "Дерево";
+            this.MenuTree.Click += new System.EventHandler(this.btnTree_Click);
+            // 
+            // treeFolder
+            // 
+            this.treeFolder.Dock = System.Windows.Forms.DockStyle.Left;
+            this.treeFolder.Location = new System.Drawing.Point(0, 24);
+            this.treeFolder.Name = "treeFolder";
+            this.treeFolder.Size = new System.Drawing.Size(228, 443);
+            this.treeFolder.TabIndex = 22;
+            // 
+            // splitter1
+            // 
+            this.splitter1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitter1.Location = new System.Drawing.Point(228, 24);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 443);
+            this.splitter1.TabIndex = 23;
+            this.splitter1.TabStop = false;
+            // 
+            // tabControl2
+            // 
+            this.tabControl2.Controls.Add(this.tabFilm);
+            this.tabControl2.Controls.Add(this.tabWriter);
+            this.tabControl2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl2.Location = new System.Drawing.Point(231, 24);
+            this.tabControl2.Name = "tabControl2";
+            this.tabControl2.SelectedIndex = 0;
+            this.tabControl2.Size = new System.Drawing.Size(711, 443);
+            this.tabControl2.TabIndex = 24;
+            // 
+            // tabFilm
+            // 
+            this.tabFilm.Controls.Add(this.dataGridView1);
+            this.tabFilm.Location = new System.Drawing.Point(4, 22);
+            this.tabFilm.Name = "tabFilm";
+            this.tabFilm.Padding = new System.Windows.Forms.Padding(3);
+            this.tabFilm.Size = new System.Drawing.Size(703, 417);
+            this.tabFilm.TabIndex = 0;
+            this.tabFilm.Text = "Фильмотека";
+            this.tabFilm.UseVisualStyleBackColor = true;
+            // 
+            // tabWriter
+            // 
+            this.tabWriter.Location = new System.Drawing.Point(4, 22);
+            this.tabWriter.Name = "tabWriter";
+            this.tabWriter.Padding = new System.Windows.Forms.Padding(3);
+            this.tabWriter.Size = new System.Drawing.Size(703, 417);
+            this.tabWriter.TabIndex = 1;
+            this.tabWriter.Text = "Редактор";
+            this.tabWriter.UseVisualStyleBackColor = true;
+            // 
+            // справкаToolStripMenuItem
+            // 
+            this.справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
+            this.справкаToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.справкаToolStripMenuItem.Text = "Справка";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(942, 489);
+            this.Controls.Add(this.tabControl2);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.treeFolder);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.mainMenu);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.mainMenu.ResumeLayout(false);
+            this.mainMenu.PerformLayout();
+            this.tabControl2.ResumeLayout(false);
+            this.tabFilm.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.FolderBrowserDialog browserDialog;
+        private System.Windows.Forms.ContextMenuStrip contextMenu;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.MenuStrip mainMenu;
+        private System.Windows.Forms.TreeView treeFolder;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.TabControl tabControl2;
+        private System.Windows.Forms.TabPage tabFilm;
+        private System.Windows.Forms.TabPage tabWriter;
+        private System.Windows.Forms.ToolStripMenuItem MenuFile;
+        private System.Windows.Forms.ToolStripMenuItem MenuScan;
+        private System.Windows.Forms.ToolStripMenuItem MenuLoad;
+        private System.Windows.Forms.ToolStripMenuItem MenuTree;
+        private System.Windows.Forms.ToolStripMenuItem справкаToolStripMenuItem;
+    }
+}
+
---
+++ Form/MainForm.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+
+
+
+namespace FilmCollection
+{
+    public partial class MainForm : Form
+    {
+        RecordCollection _videoCollection = new RecordCollection();
+        Record record = null;
+        string NodeName = "";       // хранение полного пути узла из TreeFolder
+        string SourceValue = "";
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            //string rootPath = Console.ReadLine(); var dir = new DirectoryInfo(rootPath);
+            //string samplePath = Application.StartupPath + @"\\Dirs_v6.xml";
+            //DisplayTreeView(samplePath);
+        }
+
+
+
+        private void btnScanDir_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo directory = new DirectoryInfo(@"C:\temp");
+            _videoCollection.Source = directory.FullName;
+            _videoCollection.Save();
+            //int dlinna = directory.FullName.Length;
+            //string strr;
+
+            #region Формирование списка файлов в базе XML для использования при дальнейшей проверке. Нужно ли их добавлять.
+            List<string> FileName = new List<string>();
+            XmlDocument doc = new XmlDocument();
+            doc.Load("VideoList.xml");
+
+            XmlNodeList nodeList = doc.GetElementsByTagName("Name");
+
+            foreach (XmlNode node in nodeList)
+            {
+                FileName.Add(node.ChildNodes[0].Value);
+            }
+            #endregion
+
+
+            if (directory.Exists)
+            {
+                char[] charsToTrim = { '.' };
+                foreach (FileInfo file in directory.GetFiles("*", SearchOption.AllDirectories))
+                {
+                    if (file.Name != FileName.Find(x => x.Contains(file.Name)))
+                    {
+                        record = new Record();
+                        record.Name = file.Name;
+                        record.Year = file.Name.Remove(file.Name.LastIndexOf(file.Extension), file.Extension.Length);
+                        record.Type = file.Extension.Trim(charsToTrim);
+                        record.Path = file.DirectoryName;//полный путь к файлу               // record.Path = file.Directory.Name; - папка расположения файла
+                                                         // if (-1 != file.DirectoryName.Substring(dlinna).IndexOf('\\')) strr = file.DirectoryName.Substring(dlinna + 1); //Обрезка строку путь C:\temp\1\11 -> 1\11
+
+                        _videoCollection.Add(record);
+                        _videoCollection.Save();
+                    }
+                }
+            }
+            _videoCollection = RecordCollection.Load();
+            RefreshTables();
+        }
+
+
+        private void RefreshTables()
+        {
+            List<Record> filtered = _videoCollection.VideoList;
+            if (NodeName != "")
+            {
+                filtered = filtered.FindAll(v => v.Path == SourceValue + Path.DirectorySeparatorChar + NodeName);
+                dataGridView1.DataSource = filtered;
+            }
+            else
+            {
+                dataGridView1.DataSource = filtered;
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            _videoCollection = RecordCollection.Load();
+            RefreshTables();
+
+        }
+
+
+
+
+
+
+        private void btnTree_Click(object sender, EventArgs e)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("VideoList.xml");
+
+            #region Чтение содержимого атрибута source и файла XML
+            //string SourceValue = "";
+            foreach (XmlNode xmlNode in doc.ChildNodes)
+            {
+                if (xmlNode.NodeType == XmlNodeType.Element)                           // Проверка ноды, что это элемент
+                {
+                    foreach (XmlAttribute xmlattribute in xmlNode.Attributes)
+                    {
+                        if (xmlattribute.Name == "source") SourceValue = xmlattribute.Value; // Поиск атрибута "source"
+                        if ((SourceValue != null) && (SourceValue.Length != 0)) break;
+                    }
+                }
+            }
+            #endregion
+
+            int dlinna = SourceValue.Length; // расчет длинны пути
+
+            XmlNodeList nodeList = doc.GetElementsByTagName("Path");             // Чтение элементов "Path"
+
+            treeFolder.Nodes.Clear();
+            var paths = new List<string>();                     // PopulateTreeView(treeFolder, paths, '\\');
+            //paths.Add("Фильмотека");
+
+            foreach (XmlNode node in nodeList)
+            {
+                string temp = "";
+                //if (-1 != node.ChildNodes[0].Value.Substring(dlinna).IndexOf('\\')) //Path.DirectorySeparatorChar
+                if (-1 != node.ChildNodes[0].Value.Substring(dlinna).IndexOf(Path.DirectorySeparatorChar))
+                {
+                    temp = node.ChildNodes[0].Value.Substring(dlinna + 1); //Обрезка строку путь C:\temp\1\11 -> 1\11
+                    if (temp.Length != 0) paths.Add(node.ChildNodes[0].Value.Substring(dlinna + 1));
+                }
+
+            }
+
+
+
+            //PopulateTreeView(treeFolder, paths, '\\');          // PopulateTreeView(treeFolder, paths, '\\');
+            PopulateTreeView(treeFolder, paths, Path.DirectorySeparatorChar);          // PopulateTreeView(treeFolder, paths, '\\');
+            treeFolder.AfterSelect += treeFolder_AfterSelect;
+        }
+
+
+
+        private static void PopulateTreeView(TreeView treeView, IEnumerable<string> paths, char pathSeparator)
+        {
+            TreeNode lastNode = null;
+            string subPathAgg;
+            foreach (string path in paths)
+            {
+                subPathAgg = string.Empty;
+                foreach (string subPath in path.Split(pathSeparator))
+                {
+                    subPathAgg += subPath + pathSeparator;
+                    TreeNode[] nodes = treeView.Nodes.Find(subPathAgg, true);
+                    if (nodes.Length == 0)
+                        if (lastNode == null)
+                            lastNode = treeView.Nodes.Add(subPathAgg, subPath);
+                        else
+                            lastNode = lastNode.Nodes.Add(subPathAgg, subPath);
+                    else
+                        lastNode = nodes[0];
+                }
+                lastNode = null; // This is the place code was changed
+            }
+        }
+
+
+        private void treeFolder_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            NodeName = e.Node.FullPath;         // получение полного пути ноды
+            RefreshTables();
+
+            //TreeNode ChildNode1 = new TreeNode();
+            //ChildNode1.Text = "Next Child 1";
+            //ChildNode1.ImageIndex = 1;
+            //ChildNode1.SelectedImageIndex = 1;
+            //treeView2.SelectedNode.Nodes.Add(ChildNode1);       
+        }
+
+
+        public void ChooseFolder()
+        {
+            if (browserDialog.ShowDialog() == DialogResult.OK)
+            {
+                //textBox1.Text = foldBrowsDialog.SelectedPath;
+            }
+
+        }
+
+    }
+}
+
+
+
