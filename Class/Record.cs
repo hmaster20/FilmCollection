2b5a125ee4dc7fcdcd3cd40ce9162beaff01b3f7
hmaster20 <hmaster20@gmail.com>
2016-07-10 22:34:36 +0300

    Добавлены новые поля для детализации информации о фильмах

--- Class/Record.cs
+++ Class/Record.cs
@@ -4,28 +4,49 @@ namespace FilmCollection
 {
     public class Record
     {
-        private string _name = "";
+        private string _name = "";                // Название Фильма
         public string Name
         {
             get { return _name; }
             set { _name = value; }
         }
 
-        private string _year = "";
+        private string _year = "";                // Год выпуска
         public string Year
         {
             get { return _year; }
             set { _year = value; }
         }
+        
+        private string _country = "";             // Страна выпуска
+        public string Country
+        {
+            get { return _country; }
+            set { _country = value; }
+        }
+  
+        private string _genre = "";               //жанр (комедия, боевик, вестерн)
+        public string Genre
+        {
+            get { return _genre; }
+            set { _genre = value; }
+        }
 
-        private string _type = "";
+        private string _description = "";        // Описание Фильма
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        private string _type = "";              // Тип файла
         public string Type
         {
             get { return _type; }
             set { _type = value; }
         }
 
-        private string _path = "";
+        private string _path = "";            // Путь к файлу
         public string Path
         {
             get { return _path; }
