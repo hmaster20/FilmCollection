baf74403ff7ddb254e4825cd6173299664470d6b
Serg <hmaster20@gmail.com>
2016-07-18 18:45:55 +0300

    Добавлена иконка для кнопки редактирования файла

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace FilmCollection.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap IconFileEdit {
+            get {
+                object obj = ResourceManager.GetObject("IconFileEdit", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
