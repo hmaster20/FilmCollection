e2492a1ee01259f8d3555641afbb2b848f13df55
hmaster20 <hmaster20@gmail.com>
2016-07-25 22:20:18 +0300

    Refactoring

---
+++ Class/Base/Enums.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FilmCollection
+{
+    public enum CategoryVideo
+    {
+        Film,
+        Series,
+        Cartoon,
+        Unknown
+    }
+
+    public enum GenreVideo
+    {
+        Action,
+        Vestern,
+        Comedy,
+        Unknown
+    }
+}
+
