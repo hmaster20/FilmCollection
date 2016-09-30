18c0890d32c231e33130e0d225fcdc214b29da64
Sergei <hmaster20@gmail.com>
2016-09-30 15:58:02 +0300

    Заготовка

---
+++ Class/BinSerializeHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace FilmCollection
+{
+    class BinSerializeHelper
+    {
+        //        static void serDe()
+        //        {
+        //            // создаем объект BinaryFormatter
+        //            BinaryFormatter formatter = new BinaryFormatter();
+        //            // получаем поток, куда будем записывать сериализованный объект
+        //            using (FileStream fs = new FileStream("people.dat", FileMode.OpenOrCreate)
+        //            {   Formatter.Serialize(fs, person);
+        //        }
+
+        //            // десериализация из файла people.dat
+        //            using (FileStream fs = new FileStream("people.dat", FileMode.OpenOrCreate))
+        //            {                 Person newPerson = (Person)formatter.Deserialize(fs);
+        //}
+        //                using (FileStream stream = new FileStream(Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), filename), FileMode.Create))
+        //                    serializer.Serialize(stream, objectToSerialize);
+
+        // }
+    }
+}
