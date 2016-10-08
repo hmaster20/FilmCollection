8aae1f68c8b40d95c523b6bcb3ac80168912f7d7
Sergei <hmaster20@gmail.com>
2016-10-08 13:13:12 +0300

    Exit icon to Resource

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -143,6 +143,16 @@ namespace FilmCollection.Properties {
         /// <summary>
         ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Exit {
+            get {
+                object obj = ResourceManager.GetObject("Exit", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap FilmCollection {
             get {
                 object obj = ResourceManager.GetObject("FilmCollection", resourceCulture);
