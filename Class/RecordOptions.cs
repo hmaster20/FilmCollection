0e0e9f73c2a008f77950716d6a583775d2b9407c
Sergei <hmaster20@gmail.com>
2016-10-13 22:00:09 +0300

    .

--- Class/RecordOptions.cs
+++ Class/RecordOptions.cs
@@ -10,7 +10,9 @@ namespace FilmCollection
         public static string BaseName { get { return fileName; } }
 
         public string Source { get; set; }          // Путь к корневой папке
-        
+
+        public int ID { get; set; }
+
         public int scMainSplitter { get; set; }     // Сохранение параметров сплиттеров
         public int scTabFilmSplitter { get; set; }
 
