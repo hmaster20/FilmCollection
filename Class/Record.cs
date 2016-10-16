fce3c67791cd5e83926f7defabdb82629ab0835c
Sergei <hmaster20@gmail.com>
2016-10-16 20:59:19 +0300

    .

--- Class/Record.cs
+++ Class/Record.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Serialization;
 
@@ -140,6 +141,33 @@ namespace FilmCollection
             Country_Rus _category = (Country_Rus)(Enum.Parse(typeof(Country_Rus), country));
             return _category;
         }
+        #endregion
+
+
+        #region Список ID актеров
+
+        private List<int> _actorID;     // Объявление ID актеров
+        public List<int> ActorID
+        {
+            get { return _actorID; }
+            set { _actorID = value; }
+        }
+
+        public void Add(int id)
+        {
+            ActorID.Add(id);
+        }
+
+        public void Remove(int id)
+        {
+            ActorID.Remove(id);
+        }
+
+        public void ClearID()
+        {
+            ActorID.Clear();
+        }
+
         #endregion
 
 
