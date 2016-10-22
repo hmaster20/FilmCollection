893cff1672973bf44e997509574d6a853aa00d32
Sergei <hmaster20@gmail.com>
2016-10-22 13:59:30 +0300

    .

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -173,6 +173,16 @@ namespace FilmCollection.Properties {
         /// <summary>
         ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Folder {
+            get {
+                object obj = ResourceManager.GetObject("Folder", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap grabber {
             get {
                 object obj = ResourceManager.GetObject("grabber", resourceCulture);
