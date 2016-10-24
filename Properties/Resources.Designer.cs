f9883788d229641f5b7332c828a4bf612158eedb
Sergei <hmaster20@gmail.com>
2016-10-24 21:56:46 +0300

    add icon

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -113,6 +113,16 @@ namespace FilmCollection.Properties {
         /// <summary>
         ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap dbClear {
+            get {
+                object obj = ResourceManager.GetObject("dbClear", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap dbRebuild {
             get {
                 object obj = ResourceManager.GetObject("dbRebuild", resourceCulture);
