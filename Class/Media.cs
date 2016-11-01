73cadc63a6b3558c96e43089f47a4aa486f3e544
Sergei <hmaster20@gmail.com>
2016-11-01 09:25:19 +0300

    Новый класс для храния информации о фильмах

---
+++ Class/Media.cs
@@ -0,0 +1,23 @@
+﻿// хранения медиа
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FilmCollection
+{
+    class Media
+    {
+        public int Id { get; set; }             // Уникальный идентификатор
+        public string Pic { get; set; } = "";   
+        public string Description { get; set; } = "";  // Описание файла (сюжет)
+
+        private int _year;                      // Год выпуска
+        public int Year
+        {
+            get { return _year; }
+            set { _year = (Enumerable.Range(1800, DateTime.Now.Year).Contains(value)) ? value : DateTime.Now.Year; }
+        }
+
+    }
+}
