221ff30415f435ad86f08670e375fe8f442cabca
Sergei <hmaster20@gmail.com>
2016-11-02 11:23:16 +0300

    .

--- Class/Media.cs
+++ Class/Media.cs
@@ -1,5 +1,4 @@
-﻿// хранения медиа
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +7,11 @@ namespace FilmCollection
 {
     class Media
     {
+        public Media()
+        {
+            ActorID = new List<int>();          // Создание списка ID
+        }
+
         public int Id { get; set; }             // Уникальный идентификатор
         public string Pic { get; set; } = "";   
         public string Description { get; set; } = "";  // Описание файла (сюжет)
@@ -18,6 +22,32 @@ namespace FilmCollection
             get { return _year; }
             set { _year = (Enumerable.Range(1800, DateTime.Now.Year).Contains(value)) ? value : DateTime.Now.Year; }
         }
+
+        #region Список ID актеров
+
+        private List<int> _actorID;     // Объявление ID актеров
+        public List<int> ActorID
+        {
+            get { return _actorID; }
+            set { _actorID = value; }
+        }
+
+        public void Add(int id)
+        {
+            ActorID.Add(id);
+        }
+
+        public void Remove(int id)
+        {
+            ActorID.Remove(id);
+        }
+
+        public void ClearID()
+        {
+            ActorID.Clear();
+        }
+
+        #endregion
 
     }
 }
