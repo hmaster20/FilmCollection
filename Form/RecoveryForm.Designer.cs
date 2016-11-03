cd83360e4287825bd223fad80251d2d78169d6cb
Sergei <hmaster20@gmail.com>
2016-11-03 16:30:09 +0300

    Форма восстановления

---
+++ Form/RecoveryForm.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace FilmCollection
+{
+    partial class RecoveryForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEditCancel = new System.Windows.Forms.Button();
+            this.btnEditOk = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // btnEditCancel
+            // 
+            this.btnEditCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnEditCancel.Location = new System.Drawing.Point(205, 241);
+            this.btnEditCancel.Name = "btnEditCancel";
+            this.btnEditCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnEditCancel.TabIndex = 3;
+            this.btnEditCancel.Text = "Отмена";
+            this.btnEditCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnEditOk
+            // 
+            this.btnEditOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnEditOk.Image = global::FilmCollection.Properties.Resources.attention;
+            this.btnEditOk.Location = new System.Drawing.Point(35, 241);
+            this.btnEditOk.Name = "btnEditOk";
+            this.btnEditOk.Size = new System.Drawing.Size(92, 23);
+            this.btnEditOk.TabIndex = 2;
+            this.btnEditOk.Text = "Применить";
+            this.btnEditOk.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnEditOk.UseVisualStyleBackColor = true;
+            this.btnEditOk.Click += new System.EventHandler(this.btnEditOk_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Список доступных баз:";
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(15, 25);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(300, 186);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // RecoveryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 286);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEditCancel);
+            this.Controls.Add(this.btnEditOk);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "RecoveryForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Форма восстановления";
+            this.Load += new System.EventHandler(this.RecoveryForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnEditCancel;
+        private System.Windows.Forms.Button btnEditOk;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
---
+++ Form/RecoveryForm.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+
+namespace FilmCollection
+{
+    public partial class RecoveryForm : Form
+    {
+        public string recoverBase = null;
+
+        public RecoveryForm()
+        {
+            InitializeComponent();
+        }
+
+        private void RecoveryForm_Load(object sender, EventArgs e)
+        {
+            LoadBase();
+        }
+
+        public void LoadBase()
+        {
+            //DirectoryInfo directory = new DirectoryInfo(Directory.GetCurrentDirectory());
+            //FileInfo[] files = directory.GetFiles("VideoList_*.xml", SearchOption.AllDirectories);
+
+            //for (int i = 0; i < files.Length; i++)
+            //{
+            //    // listView1.Items.Add(files[i].Name).BackColor = (files[i].Length > 2) ? Color.Green : Color.Red;
+            //    // listView1.Items.Add(files[i].Name).Font = new Font(listView1.Font, FontStyle.Strikeout);
+
+            //    if (files[i].Length < 2)
+            //    {
+            //        listView1.Items.Add(files[i].Name).Font = new Font(listView1.Font, FontStyle.Strikeout);
+            //        continue;
+            //    }
+            //    listView1.Items.Add(files[i].Name);
+            //}
+
+            //////======================================
+
+
+            listView1.View = View.Details;
+            listView1.MultiSelect = false;
+            listView1.FullRowSelect = true;
+            listView1.Columns.Add("Название", 70);
+            listView1.Columns.Add("Дата", 80);
+            listView1.Columns.Add("Время", 60);
+            listView1.Columns.Add("Размер", 60);
+
+            listView1.Items.Clear();
+
+            DirectoryInfo directory = new DirectoryInfo(Directory.GetCurrentDirectory());
+            FileInfo[] files = directory.GetFiles("VideoList_*.xml", SearchOption.AllDirectories);
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                string[] subStrings = files[i].ToString().Split('_');
+                if (files[i].Length < 2)
+                    listView1.Items.Add(subStrings[0]).Font = new Font(listView1.Font, FontStyle.Strikeout);
+                else
+                    listView1.Items.Add(subStrings[0]);
+                listView1.Items[i].SubItems.Add(subStrings[1]);
+                listView1.Items[i].SubItems.Add(subStrings[2].Remove(subStrings[2].Length - 4, 4));
+                listView1.Items[i].SubItems.Add(files[i].Length.ToString());
+            }
+        }
+
+        private void btnEditOk_Click(object sender, EventArgs e)
+        {
+            recoverBase = listView1.SelectedItems.ToString();
+            DialogResult = DialogResult.OK;
+        }
+    }
+}
