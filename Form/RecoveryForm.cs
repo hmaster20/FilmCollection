fb22d909119f4f0834cd453cb959796af5a521af
Sergei <hmaster20@gmail.com>
2016-11-05 16:33:57 +0300

    .

--- Form/RecoveryForm.cs
+++ Form/RecoveryForm.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
+using System.Linq;
 using System.Windows.Forms;
 
 
@@ -28,31 +30,82 @@ namespace FilmCollection
             //listView1.Columns.Add("Название", 70);
             listView1.Columns.Add("Дата", 80);
             listView1.Columns.Add("Время", 80);
-            listView1.Columns.Add("Размер", 60);
+            listView1.Columns.Add("Размер", 80);
 
             listView1.Items.Clear();
 
             DirectoryInfo directory = new DirectoryInfo(Directory.GetCurrentDirectory());
+
             FileInfo[] files = directory.GetFiles("VideoList_*.xml", SearchOption.AllDirectories);
 
             for (int i = 0; i < files.Length; i++)
             {
                 string[] subStrings = files[i].ToString().Split('_');
-                if (files[i].Length < 2)
+
+                if (subStrings.Length == 4)
+                {
+                    //MessageBox.Show("Test");
+                }
+                //if (subStrings[4].Contains("BAD"))
+                //{
+                //   // continue;
+                //}
+
+                // Дата файла
+                if (files[i].Length < 2 || subStrings.Length == 4)
                     listView1.Items.Add(subStrings[1]).Font = new Font(listView1.Font, FontStyle.Strikeout);
                 else
                     listView1.Items.Add(subStrings[1]);
                 //listView1.Items[i].SubItems.Add(subStrings[1]);
-                listView1.Items[i].SubItems.Add(subStrings[2].Remove(subStrings[2].Length - 4, 4));
+
+                // Время создания файла
+                if (subStrings.Length == 4)
+                {
+                    listView1.Items[i].SubItems.Add(subStrings[2]);
+                }
+                else
+                {
+                    listView1.Items[i].SubItems.Add(subStrings[2].Remove(subStrings[2].Length - 4, 4));
+                }
+               // listView1.Items[i].SubItems.Add(subStrings[2].Remove(subStrings[2].Length - 4, 4));
+
+                // Размер файла базы
                 long Size = files[i].Length;
                 string _size = "";
-
                 if (Size <= 1024) _size = Size.ToString() + " Байт";
                 else if (Size > 1024 && Size <= 1024 * 1024) _size = (Math.Round((float)Size / 1024)).ToString() + " КБ";
                 else if (Size > 1024 * 1024) _size = Math.Round(((double)Size / (1024 * 1024)), 1).ToString() + " МБ";
-
                 listView1.Items[i].SubItems.Add(_size);
             }
+
+
+
+            //IEnumerable<FileInfo> Bases = directory.GetFiles("VideoList_*.xml").Where(f => !f.Name.Contains("BAD"));
+            //int BaseCount = Bases.Count();
+
+            //for (int i = 0; i < BaseCount; i++)
+            //{
+            //    string[] subStrings = files[i].ToString().Split('_');
+
+            //    // Дата файла
+            //    if (files[i].Length < 2)
+            //        listView1.Items.Add(subStrings[1]).Font = new Font(listView1.Font, FontStyle.Strikeout);
+            //    else
+            //        listView1.Items.Add(subStrings[1]);
+            //    //listView1.Items[i].SubItems.Add(subStrings[1]);
+
+            //    // Время создания файла
+            //    listView1.Items[i].SubItems.Add(subStrings[2].Remove(subStrings[2].Length - 4, 4));
+
+            //    // Размер файла базы
+            //    long Size = files[i].Length;
+            //    string _size = "";
+            //    if (Size <= 1024) _size = Size.ToString() + " Байт";
+            //    else if (Size > 1024 && Size <= 1024 * 1024) _size = (Math.Round((float)Size / 1024)).ToString() + " КБ";
+            //    else if (Size > 1024 * 1024) _size = Math.Round(((double)Size / (1024 * 1024)), 1).ToString() + " МБ";
+            //    listView1.Items[i].SubItems.Add(_size);
+            //}
+            
         }
 
         private void btnEditOk_Click(object sender, EventArgs e)
