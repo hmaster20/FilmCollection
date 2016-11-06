4f8d1429cb2ba60a1d961e7b043974c74d8e7453
Sergei <hmaster20@gmail.com>
2016-11-06 22:01:17 +0300

    .

--- Class/_Record.cs
+++ Class/_Record.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Serialization;
 
 namespace FilmCollection
 {
+   
+
+
     public class Record
     {
         public bool Visible { get; set; }       // Видимость записи
@@ -111,33 +115,33 @@ namespace FilmCollection
         #endregion
 
 
-        #region Обработка Страны
-        private Country_Rus _country;   // Страна
-        [XmlIgnore]
-        public Country_Rus Country
-        {
-            get { return _country; }
-            set { _country = (value < 0) ? Country_Rus.Россия : value; }
-        }
+        //#region Обработка Страны
+        //private Country_Rus _country;   // Страна
+        //[XmlIgnore]
+        //public Country_Rus Country
+        //{
+        //    get { return _country; }
+        //    set { _country = (value < 0) ? Country_Rus.Россия : value; }
+        //}
 
-        public string CountryString
-        {
-            get { return CountryToString(Country); }
-            set { Country = StringToCountry(value); }
-        }
+        //public string CountryString
+        //{
+        //    get { return CountryToString(Country); }
+        //    set { Country = StringToCountry(value); }
+        //}
 
-        public static string CountryToString(Country_Rus country)
-        {
-            Country_Rus _category = (Country_Rus)((int)country);
-            return _category.ToString();
-        }
+        //public static string CountryToString(Country_Rus country)
+        //{
+        //    Country_Rus _category = (Country_Rus)((int)country);
+        //    return _category.ToString();
+        //}
 
-        public static Country_Rus StringToCountry(string country)
-        {
-            Country_Rus _category = (Country_Rus)(Enum.Parse(typeof(Country_Rus), country));
-            return _category;
-        }
-        #endregion
+        //public static Country_Rus StringToCountry(string country)
+        //{
+        //    Country_Rus _category = (Country_Rus)(Enum.Parse(typeof(Country_Rus), country));
+        //    return _category;
+        //}
+        //#endregion
 
         
         #region Сравнения
@@ -171,10 +175,10 @@ namespace FilmCollection
             return string.Compare(a.DirName, b.DirName);
         }
 
-        public static int CompareByCountry(Record a, Record b)  // Сравнение по стране
-        {
-            return string.Compare(a.CountryString, b.CountryString);
-        }
+        //public static int CompareByCountry(Record a, Record b)  // Сравнение по стране
+        //{
+        //    return string.Compare(a.CountryString, b.CountryString);
+        //}
 
         public static int CompareByGenre(Record a, Record b) // Сравнение по жанру
         {
