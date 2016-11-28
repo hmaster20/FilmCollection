4284b9d23733e27bca3a918052f07a2c5a927fbd
Sergei <hmaster20@gmail.com>
2016-11-28 10:15:48 +0300

    .

---
+++ Class/Combine.cs
@@ -0,0 +1,29 @@
+﻿// Класс для объединения Media (информации о фильме)  и Record (самого фала виедо)
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FilmCollection
+{
+    class Combine
+    {
+        public Combine()
+        {
+            Record = new List<Record>();
+        }
+
+        public Media _Media { get; set; }
+        public List<Record> Record { get; set; }
+
+
+
+        public string FileName { get { return "test"; } }
+
+        public string Name { get { return _Media.Name; } }
+     
+
+
+
+    }
+}
