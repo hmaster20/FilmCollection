93923a35bbd62ddc303555f62d16675512cd6cf3
Sergei <hmaster20@gmail.com>
2016-11-30 14:55:18 +0300

    .

--- Class/Combine.cs
+++ Class/Combine.cs
@@ -1,4 +1,4 @@
-﻿// Класс для объединения Media (информации о фильме)  и Record (самого фала виедо)
+﻿// Класс для объединения Media (информации о фильме)  и Record (самого фала видео)
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,23 +6,25 @@ using System.Text;
 
 namespace FilmCollection
 {
-    class Combine
+    public class Combine
     {
         public Combine()
         {
             Record = new List<Record>();
         }
 
-        public Media _Media { get; set; }
+        public Media media { get; set; }
         public List<Record> Record { get; set; }
 
 
 
-        public string FileName { get { return "test"; } }
+        //public string FileName { get { return "test"; } }
 
-        public string Name { get { return _Media.Name; } }
-     
+        //public string Name { get { return media.Name; } }
 
+
+        // создаем количество файлов и меди поровну
+        // как только находим сериал, то создаем отдельный медиа и в него добавляем файлы, остальные медиа уничтожаем.
 
 
     }
