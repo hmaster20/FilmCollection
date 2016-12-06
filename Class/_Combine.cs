1fdb765c42efae0fce57308bc4f12b4881fbc80d
Sergei <hmaster20@gmail.com>
2016-12-06 17:13:38 +0300

    rename

---
+++ Class/_Combine.cs
@@ -0,0 +1,20 @@
+﻿// Класс для объединения Media (информации о фильме) и Record (самого файла видео)
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FilmCollection
+{
+    public class Combine
+    {
+        public Combine()
+        {
+            recordList = new List<Record>();
+            media = new Media();
+        }
+
+        public Media media { get; set; }                // создание объекта Media
+        public List<Record> recordList { get; set; }    // хранение файлов принадлежащих Media
+    }
+}
