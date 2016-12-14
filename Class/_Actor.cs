279d0c46dc8b72f7b5c95eee718bcc4c46d3bc72
Sergei <hmaster20@gmail.com>
2016-12-14 09:58:09 +0300

    .

--- Class/_Actor.cs
+++ Class/_Actor.cs
@@ -20,6 +20,8 @@ namespace FilmCollection
         public string DateOfBirth { get; set; }   // дата рождения
         public string DateOfDeath { get; set; }   // дата смерти
 
+        public string link { get; set; }            // ссылка на страницу с биографией
+
         public override string ToString()
         {
             return FIO;
