befd14232198aa0bb7662c79595827beb2d33922
hmaster20 <hmaster20@gmail.com>
2017-01-25 21:45:05 +0300

    добавлено описание

--- Class/RecordCollection.cs
+++ Class/RecordCollection.cs
@@ -6,6 +6,9 @@ using System.Linq;
 
 namespace FilmCollection
 {
+    /// <summary>
+    /// Класс управления коллекцией фильмотеки
+    /// </summary>
     public class RecordCollection
     {
         [XmlElement]
