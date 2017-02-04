3e4ce78219d1b29dfee48d00a0a51eacc0ceef6e
hmaster20 <hmaster20@gmail.com>
2017-02-04 15:24:02 +0300

    добавление в ресурсы иконки

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -303,6 +303,16 @@ namespace FilmCollection.Properties {
         /// <summary>
         ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Report {
+            get {
+                object obj = ResourceManager.GetObject("Report", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap resetFiltr {
             get {
                 object obj = ResourceManager.GetObject("resetFiltr", resourceCulture);
