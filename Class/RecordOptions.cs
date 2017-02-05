3b70eed3edb978e0b62cb1d0dce8e0e83684bc9d
hmaster20 <hmaster20@gmail.com>
2017-02-05 22:19:54 +0300

    добавлен метод выдачи пути к каталогу изображений

--- Class/RecordOptions.cs
+++ Class/RecordOptions.cs
@@ -1,19 +1,22 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Xml.Serialization;
+using System.Windows.Forms;
 
 namespace FilmCollection
 {
     /// <summary>Параметры настроек программы</summary>
     public class RecordOptions
     {
+        /// <summary>Файл базы коллекции "VideoList.xml"</summary>
         [XmlIgnore]
-        public static string BaseName { get; } = "VideoList.xml";   // Файл базы коллекции // string BaseName = "VideoList.xml"
+        public static string BaseName { get; } = "VideoList.xml";
 
+        /// <summary>Метод генерирует массив вида ".avi", ".mkv", ".mp4", ".wmv", ".webm", ".rm", ".mpg", ".mpeg", ".flv", ".divx"</summary>
         public static List<string> FormatAdd()
         {
             List<string> FormatAdd = new List<string>();
-
             foreach (var item in Enum.GetValues(typeof(MediaFormat)))
             {
                 FormatAdd.Add("." + item.ToString());
@@ -21,26 +24,26 @@ namespace FilmCollection
             return FormatAdd;
         }
 
+        /// <summary>Метод генерирует строку вида "Видео (*.avi, *.mkv, *.mp4, ..)|*.avi;*.mkv;*.mp4;*.wmv;*.webm;*.rm;*.mpg;*.flv;*.divx|Все файлы (*.*) | *.*"</summary>
         public static string FormatOpen()
         {
-            //string FormatOpen { get; } = "Видео (*.avi, *.mkv, *.mp4, ..)|*.avi;*.mkv;*.mp4;*.wmv;*.webm;*.rm;*.mpg;*.flv;*.divx|Все файлы (*.*) | *.*";
-
             string FormatOpen = "Видео(*.avi, *.mkv, *.mp4, ..) |";
             foreach (var item in Enum.GetValues(typeof(MediaFormat)))
             {
                 FormatOpen = FormatOpen + "*." + item.ToString() + ";";
             }
-            FormatOpen = FormatOpen.Remove(FormatOpen.Length - 1, 1);
-            FormatOpen = FormatOpen + "|Все файлы (*.*) | *.*";
-
+            FormatOpen = FormatOpen.Remove(FormatOpen.Length - 1, 1) + "|Все файлы (*.*) | *.*";
             return FormatOpen;
         }
 
+        /// <summary>Каталог изображений</summary>
+        public static string PicsFolder()
+        {
+            return  Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "Pics");
+        }
 
-
-
-
-        public string Source { get; set; }          // Путь к корневой папке
+        /// <summary>Путь к корневой папке</summary>
+        public string Source { get; set; } 
 
         public int scMainSplitter { get; set; }     // Сохранение параметров сплиттеров
         public int scTabFilmSplitter { get; set; }
@@ -49,11 +52,7 @@ namespace FilmCollection
 
         [XmlAttribute]
         public string FormState { get; set; }       // Поле сохранения состояния главной формы
-
-
-
-
-
+ 
 
 
         // Тестовое ПОЛЕ
@@ -64,7 +63,5 @@ namespace FilmCollection
             get { return _txt; }
             set { _txt = value; }
         }
-
-
     }
 }
