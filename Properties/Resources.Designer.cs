acfdf43c8d0918afea700a2c609e432ec6cf7b08
Sergei <hmaster20@gmail.com>
2017-02-23 23:18:31 +0300

    добавлены ресурсы

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -323,6 +323,16 @@ namespace FilmCollection.Properties {
         /// <summary>
         ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap help_run {
+            get {
+                object obj = ResourceManager.GetObject("help_run", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap play {
             get {
                 object obj = ResourceManager.GetObject("play", resourceCulture);
