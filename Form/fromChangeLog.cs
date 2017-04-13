fbfc31a4c09e617e9eb9448215d052edf8d31102
Sergei <hmaster20@gmail.com>
2017-04-13 22:30:05 +0300

    добавлена форма вывода информации о журнале событий

---
+++ Form/fromChangeLog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FilmCollection
+{
+    public partial class fromChangeLog : Form
+    {
+        public fromChangeLog()
+        {
+            InitializeComponent();
+
+            tbLog.Text = Properties.Resources.ChangeLog;
+        }
+    }
+}
