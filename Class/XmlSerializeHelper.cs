e9e4d6a8878514592e5a745c3fb8cc3e0a8a205f
Sergei <hmaster20@gmail.com>
2017-04-25 15:44:58 +0300

    Работа с файлом базы в ОЗУ

--- Class/XmlSerializeHelper.cs
+++ Class/XmlSerializeHelper.cs
@@ -3,31 +3,24 @@ using System.IO;
 using System.Windows.Forms;
 using System.Xml.Serialization;
 
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Formatters.Binary;
-
 namespace FilmCollection
 {
     public static class XmlSerializeHelper
     {
         static MemoryStream streamCollection = new MemoryStream();
 
+
+        #region Сохранение путем сериализации
+
         /// <summary>Сериализация (сохранение объекта) в файл</summary>
-        /// <param name="filename">Имя файла RecordOptions.BaseName</param>
-        /// <param name="objectToSerialize">Класс RecordCollection</param>
-        /// <returns></returns>
         public static bool SerializeAndSave(string filename, object objectToSerialize)
         {
-            XmlSerializer serializer = new XmlSerializer(objectToSerialize.GetType());
             try
             {
-                string FileWithPath = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), filename);
-
-                using (FileStream stream = new FileStream(FileWithPath, FileMode.Create))
+                using (FileStream stream = new FileStream((Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), filename)), FileMode.Create))
                 {
-                    serializer.Serialize(stream, objectToSerialize);
+                    new XmlSerializer(objectToSerialize.GetType()).Serialize(stream, objectToSerialize);
                 }
-
                 SerializeAndSaveMemory(objectToSerialize);
             }
             catch (Exception e)
@@ -38,20 +31,12 @@ namespace FilmCollection
             return true;
         }
 
-
         public static void SerializeAndSaveMemory(object objectToSerialize)
         {
-            // MemoryStream streamCollection = SerializeToStream(objectToSerialize);
-
-            XmlSerializer serializer = new XmlSerializer(objectToSerialize.GetType());
             try
             {
-                //using (MemoryStream stream = new MemoryStream())
-                //{
-                //    serializer.Serialize(stream, objectToSerialize);
-                //}
-               streamCollection.Position = 0;
-                serializer.Serialize(streamCollection, objectToSerialize);
+                streamCollection.Position = 0;
+                new XmlSerializer(objectToSerialize.GetType()).Serialize(streamCollection, objectToSerialize);
             }
             catch (Exception e)
             {
@@ -60,17 +45,22 @@ namespace FilmCollection
         }
 
 
+
+        #endregion
+
+
+        #region Загрузка путем десериализации
+
+        /// <summary>Селектор загрузки</summary>
         public static RecordCollection LoadSelector()
         {
-            if (streamCollection != null || streamCollection.Length!= 0)
+            if (streamCollection != null && streamCollection.Length != 0)
             {
-                return LoadAndDeserializeMemory<RecordCollection>(); 
-               // RecordCollection RC = (RecordCollection)DeserializeFromStream(streamCollection);
-               // return RC;
+                return LoadAndDeserializeMemory<RecordCollection>();
             }
             else
             {
-                return LoadAndDeserialize<RecordCollection>("");
+                return LoadAndDeserialize<RecordCollection>(RecordOptions.BaseName);
             }
         }
 
@@ -79,9 +69,11 @@ namespace FilmCollection
         {
             if (!File.Exists(filename))
                 throw new Exception("File not exist");
-            //long length = new FileInfo(filename).Length;
+
+
             if ((new FileInfo(filename).Length) < 200)
                 throw new Exception("Некорректный файл XML");
+
 
             XmlSerializer serializer = new XmlSerializer(typeof(T));
 
@@ -94,57 +86,25 @@ namespace FilmCollection
                     return t;
                 }
             }
-            catch (Exception ex)
-            {
-                //throw new Exception("Error during deserializing");
-                throw new Exception(ex.Message + "\nПричина: " + ex.InnerException.Message);
-            }
+            catch (Exception ex) { throw new Exception(ex.Message + "\nПричина: " + ex.InnerException.Message); }
         }
 
-
-        //public static T LoadAndDeserializeMemory<T>(MemoryStream streamCollection)
         public static T LoadAndDeserializeMemory<T>()
         {
             if (streamCollection == null)
-            {
                 throw new Exception("Некорректный поток");
-            }
+
 
             XmlSerializer serializer = new XmlSerializer(typeof(T));
 
             try
             {
-                //streamCollection.Position = 0;
                 streamCollection.Seek(0, SeekOrigin.Begin);
                 return (T)serializer.Deserialize(streamCollection);
             }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message + "\nПричина: " + ex.InnerException.Message + "\nLoadAndDeserializeMemory");
-            }
+            catch (Exception ex) { throw new Exception(ex.Message + "\nПричина: " + ex.InnerException.Message); }
         }
-
-
-
-
-
-        public static MemoryStream SerializeToStream(object o)
-        {
-            MemoryStream stream = new MemoryStream();
-            IFormatter formatter = new BinaryFormatter();
-            formatter.Serialize(stream, o);
-            return stream;
-        }
-
-        public static object DeserializeFromStream(MemoryStream stream)
-        {
-            IFormatter formatter = new BinaryFormatter();
-            stream.Seek(0, SeekOrigin.Begin);
-            object o = formatter.Deserialize(stream);
-            return o;
-        }
-
+        #endregion
 
     }
 }
-
