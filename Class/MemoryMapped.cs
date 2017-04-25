ba685d3a7bf4beb8b57f50bbcf85496f0f5d3b99
Sergei <hmaster20@gmail.com>
2017-04-25 15:49:07 +0300

    Добавлен тестовый класс

---
+++ Class/MemoryMapped.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.MemoryMappedFiles;
+using System.IO;
+
+namespace FilmCollection.Class
+{
+    class MemoryMapped
+    {
+        void openFile()
+        {
+            MemoryMappedFile mmf = MemoryMappedFile.CreateFromFile(RecordOptions.BaseName, FileMode.Open, "VideoList");
+
+
+
+        }
+    }
+}
