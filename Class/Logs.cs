463e65e881474c9a6cbaba82735161f74379ac66
Sergei <hmaster20@gmail.com>
2017-05-16 16:27:21 +0300

    сделан класс для формирования журнала событий

---
+++ Class/Logs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace FilmCollection
+{
+    public static class Logs
+    {
+
+        public static void Log(string message, Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+
+            File.AppendAllText("log.txt", "\r\n-------------------");
+            File.AppendAllText("log.txt", (string.Format($"\r\n{DateTime.Now.ToString("yyyy.MM.dd_HH:mm:ss")}\r\n")));
+            File.AppendAllText("log.txt", message);
+            File.AppendAllText("log.txt", "\r\n");
+            File.AppendAllText("log.txt", ex.Message);
+        }
+    }
+}
