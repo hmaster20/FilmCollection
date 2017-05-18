e44622f52ee70495f6491d7fe0fdb5788aeced4f
Sergei <hmaster20@gmail.com>
2017-05-18 17:39:22 +0300

    доработан конструктор для отображения данных

--- Form/ucMediaInfo.cs
+++ Form/ucMediaInfo.cs
@@ -14,6 +14,7 @@ namespace FilmCollection
     public partial class ucMediaInfo : UserControl
     {
         public Record record { get; set; }
+        public RecordCollection _videoCollection { get; set; }
 
         public ucMediaInfo()
         {
@@ -21,8 +22,28 @@ namespace FilmCollection
         }
 
         public ucMediaInfo(Record _record)
-        {          
+        {
+            InitializeComponent();
+
             this.record = _record;
+            this._videoCollection = RecordCollection.GetInstance();
+
+
+            if (record != null)
+            {
+                // Панель описания
+                tbfName.Text = record.mName;
+                tbfDesc.Text = record.mDescription;
+                tbfYear.Text = Convert.ToString(record.mYear);
+                tbfCountry.Text = record.mCountry;
+                GetPic(record.combineLink.media);
+                btnPlay.Enabled = true;
+                lbActors.Items.Clear();
+                if (record.combineLink.media.ActorListID != null)
+                    foreach (int ListID in record.combineLink.media.ActorListID)
+                        if (_videoCollection.ActorList.Exists(act => act.id == ListID))
+                            lbActors.Items.Add(_videoCollection.ActorList.FindLast(act => act.id == ListID));
+            }
         }
 
         private void btnPlay_Click(object sender, EventArgs e)
@@ -41,6 +62,20 @@ namespace FilmCollection
             }
         }
 
-   
+        private void GetPic(Media _media)
+        {
+            if (_media != null)
+            {
+                if (File.Exists(_media.GetFilename))
+                {
+                    Image image = Image.FromFile(_media.GetFilename);
+                    pbImage.Image = (image.Height > 300)
+                        ? image.GetThumbnailImage(300 * image.Width / image.Height, 300, null, IntPtr.Zero)
+                        : image;
+                }
+                else pbImage.Image = null;
+            }
+        }
+
     }
 }
