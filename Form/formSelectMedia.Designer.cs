61ee7142d5e3dea99070c45ea06a45787dbdbc28
Sergei <hmaster20@gmail.com>
2017-05-19 14:01:50 +0300

    test selecta

--- Form/formSelectMedia.Designer.cs
+++ Form/formSelectMedia.Designer.cs
@@ -29,13 +29,15 @@
         private void InitializeComponent()
         {
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.ucMediaInfo1 = new FilmCollection.ucMediaInfo();
-            this.btnOk = new System.Windows.Forms.Button();
-            this.btnCancel = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOk = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
+            this.lCountry = new System.Windows.Forms.Label();
+            this.lYear = new System.Windows.Forms.Label();
+            this.SelectMediaInfo = new FilmCollection.ucMediaInfo();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -56,13 +58,25 @@
             // 
             // splitContainer1.Panel2
             // 
-            this.splitContainer1.Panel2.Controls.Add(this.ucMediaInfo1);
+            this.splitContainer1.Panel2.Controls.Add(this.SelectMediaInfo);
             this.splitContainer1.Size = new System.Drawing.Size(783, 559);
-            this.splitContainer1.SplitterDistance = 413;
+            this.splitContainer1.SplitterDistance = 397;
             this.splitContainer1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(397, 559);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.lYear);
+            this.panel1.Controls.Add(this.lCountry);
             this.panel1.Controls.Add(this.btnCancel);
             this.panel1.Controls.Add(this.btnOk);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
@@ -70,26 +84,6 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(783, 56);
             this.panel1.TabIndex = 1;
-            // 
-            // ucMediaInfo1
-            // 
-            this.ucMediaInfo1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.ucMediaInfo1.Location = new System.Drawing.Point(0, 0);
-            this.ucMediaInfo1.Name = "ucMediaInfo1";
-            this.ucMediaInfo1.record = null;
-            this.ucMediaInfo1.Size = new System.Drawing.Size(366, 559);
-            this.ucMediaInfo1.TabIndex = 0;
-            // 
-            // btnOk
-            // 
-            this.btnOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.btnOk.Location = new System.Drawing.Point(689, 17);
-            this.btnOk.Name = "btnOk";
-            this.btnOk.Size = new System.Drawing.Size(75, 23);
-            this.btnOk.TabIndex = 0;
-            this.btnOk.Text = "Ок";
-            this.btnOk.UseVisualStyleBackColor = true;
             // 
             // btnCancel
             // 
@@ -102,14 +96,17 @@
             this.btnCancel.Text = "Отмена";
             this.btnCancel.UseVisualStyleBackColor = true;
             // 
-            // listBox1
+            // btnOk
             // 
-            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(0, 0);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(413, 559);
-            this.listBox1.TabIndex = 0;
+            this.btnOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOk.Location = new System.Drawing.Point(689, 17);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 0;
+            this.btnOk.Text = "Ок";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
             // 
             // panel2
             // 
@@ -129,6 +126,33 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Необходимо выбрать одну из записей отображаемых в списке ниже:";
             // 
+            // lCountry
+            // 
+            this.lCountry.AutoSize = true;
+            this.lCountry.Location = new System.Drawing.Point(24, 22);
+            this.lCountry.Name = "lCountry";
+            this.lCountry.Size = new System.Drawing.Size(45, 13);
+            this.lCountry.TabIndex = 1;
+            this.lCountry.Text = "lCountry";
+            // 
+            // lYear
+            // 
+            this.lYear.AutoSize = true;
+            this.lYear.Location = new System.Drawing.Point(122, 22);
+            this.lYear.Name = "lYear";
+            this.lYear.Size = new System.Drawing.Size(31, 13);
+            this.lYear.TabIndex = 1;
+            this.lYear.Text = "lYear";
+            // 
+            // SelectMediaInfo
+            // 
+            this.SelectMediaInfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SelectMediaInfo.Location = new System.Drawing.Point(0, 0);
+            this.SelectMediaInfo.Name = "SelectMediaInfo";
+            this.SelectMediaInfo.record = null;
+            this.SelectMediaInfo.Size = new System.Drawing.Size(382, 559);
+            this.SelectMediaInfo.TabIndex = 0;
+            // 
             // formSelectMedia
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -137,7 +161,7 @@
             this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "formSelectMedia";
             this.Text = "formSelectMedia";
             this.splitContainer1.Panel1.ResumeLayout(false);
@@ -145,6 +169,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             this.ResumeLayout(false);
@@ -155,11 +180,13 @@
 
         private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.Panel panel1;
-        private ucMediaInfo ucMediaInfo1;
+        private ucMediaInfo SelectMediaInfo;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button btnCancel;
         private System.Windows.Forms.Button btnOk;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lYear;
+        private System.Windows.Forms.Label lCountry;
     }
 }
--- Form/formSelectMedia.cs
+++ Form/formSelectMedia.cs
@@ -11,7 +11,8 @@ namespace FilmCollection
 {
     public partial class formSelectMedia : Form
     {
-        public List<Media> MList { get; set; }
+        //public List<Media> MList { get; set; }
+        public Media media = null;
 
         public formSelectMedia()
         {
@@ -20,7 +21,34 @@ namespace FilmCollection
 
         public formSelectMedia(List<Media> MList)
         {
+            InitializeComponent();
 
+            MList.ForEach(x=> listBox1.Items.Add(x));
+
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            Media media = (Media)listBox1.SelectedItem;
+            DialogResult = DialogResult.OK;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            Media media = (Media)listBox1.SelectedItem;
+
+            SelectMediaInfo.update(media);
+
+            //ucMediaInfo uc = new ucMediaInfo(media);             
+            //uc.Show();
+            //uc.Refresh();
+
+           // lCountry.Text = media.CountryString;
+          //  lYear.Text = media.Year.ToString();
+                           
         }
     }
 }
+
+               
