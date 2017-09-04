6c96773b70c8023e5b30daadf0a05dbbdbc548c4
hmaster <hmaster20@gmail.com>
2017-09-04 22:06:03 +0300

    модуль восстановления базы

---
+++ Form/formReindexer.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace FilmCollection
+{
+    partial class formReindexer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnParse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnParse
+            // 
+            this.btnParse.Location = new System.Drawing.Point(366, 73);
+            this.btnParse.Name = "btnParse";
+            this.btnParse.Size = new System.Drawing.Size(75, 23);
+            this.btnParse.TabIndex = 0;
+            this.btnParse.Text = "btnParse";
+            this.btnParse.UseVisualStyleBackColor = true;
+            this.btnParse.Click += new System.EventHandler(this.btnParse_Click);
+            // 
+            // formReindexer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 363);
+            this.Controls.Add(this.btnParse);
+            this.Name = "formReindexer";
+            this.Text = "Reindexer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnParse;
+    }
+}
---
+++ Form/formReindexer.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace FilmCollection
+{
+    public partial class formReindexer : Form
+    {
+        public formReindexer()
+        {
+            InitializeComponent();
+        }
+
+        public formReindexer(RecordCollection _videoCollection)
+        {
+            InitializeComponent();
+        }
+
+        private void btnParse_Click(object sender, EventArgs e)
+        {
+            string file = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), RecordOptions.BaseName);
+
+
+            XDocument xml = XDocument.Load(file);
+
+            var nodes = (from n in xml.Descendants("media")
+                         select n.Element("Id").Value
+                 ).ToList();
+
+            List<string> nn = new List<string>();
+            nn = nodes;
+
+
+
+            var duplicatesSs = nodes.GroupBy(s => s)
+                .SelectMany(grp => grp.Skip(1)).ToList();
+
+            var querys = nodes.GroupBy(x => x)
+                .Where(g => g.Count() > 1)
+                .Select(y => y.Key)
+                .ToList();
+
+            var queryss = nodes.GroupBy(x => x)
+              .Where(g => g.Count() > 1)
+              .Select(y => new { Element = y.Key, Counter = y.Count() })
+              .ToList();
+
+            var duplicateKeys = nn.GroupBy(x => x)
+                        .Where(group => group.Count() > 1)
+                        .Select(group => group.Key)
+                        .ToList();
+
+
+
+            XDocument xmlErr = XDocument.Load(file);
+
+            foreach (var item in querys)
+            {
+                var nodesErr = (from n in xmlErr.Descendants("media")
+                                where n.Element("Id").Value == item //"0"
+                                select n.Element("Name").Value
+                                ).ToList();
+
+                foreach (var node in nodesErr)
+                {
+                    Console.WriteLine(node.Trim());
+                }
+
+            }
+
+
+
+
+
+
+
+            XDocument xml2 = XDocument.Load(file);
+
+            var ActorNodes = (from n in xml2.Descendants("Actor")
+                         select n.Element("id").Value
+                         ).ToList();
+
+            List<string> nn2 = new List<string>();
+            nn2 = ActorNodes;
+
+            var duplicateKeys2 = nn2.GroupBy(x => x)
+                .Where(group => group.Count() > 1)
+                .Select(group => group.Key);
+
+
+
+
+            //foreach (var item in nodes)
+            //{
+            //    Console.WriteLine(item.Trim());
+            //}
+
+
+
+            //XmlTextReader reader = new XmlTextReader(file);
+            //while (reader.Read())
+            //{
+            //    if (reader.ReadToDescendant("title"))
+            //    {
+            //        reader.Read();//this moves reader to next node which is text 
+            //                      //result = reader.Value; //this might give value than 
+            //                      //break;
+            //        Console.WriteLine(reader.Value.Trim());
+            //    }
+            //}
+        }
+    }
+}
