020df6d62de1a8f7b24acc72632a6adbfa1203cd
hmaster20 <hmaster20@gmail.com>
2017-09-20 17:49:15 +0300

    ref class

--- Class/Main/_Sources.cs
+++ Class/Main/_Sources.cs
@@ -5,21 +5,10 @@ using System.Text;
 
 namespace FilmCollection
 {
+    /// <summary>Источник файлов (коллекции) - по сути путь к каталогу базы</summary>
     public class Sources
     {
         public int Id { get; set; }
         public string Source { get; set; }
-
-
-        //public Sources() { }
-        //public Sources(int _id, string _source)
-        //{
-        //    id = _id;
-        //    source = _source;
-        //}
-        //private int id { get; set; }
-        //public int Id { get { return id; } }
-        //private string source { get; set; }
-        //public string Source { get { return source; } }
     }
 }
