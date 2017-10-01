508f55349fba1965707eb4e2209dbc537690045b
hmaster <hmaster20@gmail.com>
2017-10-01 23:02:52 +0300

    схеиа актеров фильма

--- Form/ucChart.cs
+++ Form/ucChart.cs
@@ -12,12 +12,67 @@ namespace FilmCollection
 {
     public partial class ucChart : UserControl
     {
-        public ucChart()
-        {
-            InitializeComponent();
+        //public ucChart()
+        //{
+        //    InitializeComponent();
+        //    ShowCharts();
+        //}
 
-            ShowCharts();
+        public ucChart() => InitializeComponent();
+
+
+        public void update(Record _record)
+        {
+            //this.record = _record;
+            //ShowCharts();
+
+            Dictionary<string, string> rcDic = new Dictionary<string, string>();
+
+            RecordCollection _videoCollection = RecordCollection.GetInstance();
+
+            List<Actor> acts = new List<Actor>();
+
+            if (_record.combineLink.media.ActorListID != null)
+                foreach (int ListID in _record.combineLink.media.ActorListID)
+                    if (_videoCollection.ActorList.Exists(act => act.id == ListID))
+                        acts.Add(_videoCollection.ActorList.FindLast(act => act.id == ListID));
+
+            if (acts.Count > 0)
+            {
+                foreach (Actor act in acts)
+                {
+                    //rcDic.Add(_record.combineLink.media.Name, act.FIO);
+                    rcDic.Add(act.FIO, _record.combineLink.media.Name);
+                }
+                Show(rcDic);
+            }
         }
+
+        private void Show(Dictionary<string, string> rcDic)
+        {
+            Microsoft.Msagl.GraphViewerGdi.GViewer viewer = new Microsoft.Msagl.GraphViewerGdi.GViewer();
+            Microsoft.Msagl.Drawing.Graph graph = new Microsoft.Msagl.Drawing.Graph("graph");
+
+            foreach (var item in rcDic)
+            {
+                graph.AddEdge(item.Value, item.Key);
+            }
+            //string f = rcDic.Keys.First();
+            string f = rcDic.Values.First();
+
+            graph.FindNode(f).Attr.FillColor = Microsoft.Msagl.Drawing.Color.Gray;
+            Microsoft.Msagl.Drawing.Node c = graph.FindNode(f);
+            c.Attr.FillColor = Microsoft.Msagl.Drawing.Color.PaleGreen;
+            c.Attr.Shape = Microsoft.Msagl.Drawing.Shape.Diamond;
+            viewer.Graph = graph;
+            this.SuspendLayout();
+            viewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Controls.Add(viewer);
+            this.ResumeLayout();
+        }
+
+
+
 
         private void ShowCharts()
         {
@@ -36,6 +91,7 @@ namespace FilmCollection
             Microsoft.Msagl.Drawing.Node c = graph.FindNode("C");
             c.Attr.FillColor = Microsoft.Msagl.Drawing.Color.PaleGreen;
             c.Attr.Shape = Microsoft.Msagl.Drawing.Shape.Diamond;
+
             //bind the graph to the viewer 
             viewer.Graph = graph;
             //associate the viewer with the form 
