5d8c55e7cfa18d3a44c86860181ff831cbed9933
hmaster <hmaster20@gmail.com>
2017-10-01 23:01:59 +0300

    исправлена ошибка отображения файлов выбранной ноды

--- Form/formMain.cs
+++ Form/formMain.cs
@@ -958,6 +958,11 @@ namespace FilmCollection
                 //            ? filtered.FindAll(v => v.Path == RCollection.Options.Source + Path.DirectorySeparatorChar + node)
                 //            // отобразить все файлы в т.ч. и вложенные
                 //            : filtered = filtered.FindAll(v => v.Path.StartsWith(RCollection.Options.Source + Path.DirectorySeparatorChar + node));
+
+                //string selecteNode = RCollection.SourceList[0].Source + Path.DirectorySeparatorChar + node;
+                //filtered = (!ShowAllFiles) ? filtered.FindAll(v => v.Path == selecteNode) : filtered.FindAll(v => v.Path.StartsWith(selecteNode));
+                //filtered = (!ShowAllFiles) ? filtered.FindAll(v => v.Path == node) : filtered.FindAll(v => v.Path.StartsWith(node));
+                filtered = (!ShowAllFiles) ? filtered.FindAll(v => (RCollection.SourceList[0].Source + v.Path) == node) : filtered.FindAll(v => (RCollection.SourceList[0].Source + v.Path).StartsWith(node));
             }
 
             if (isTabFilm())
