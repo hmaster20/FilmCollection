309751f9880c4b0a949573ccca7c50e3fd691bda
hmaster20 <hmaster20@gmail.com>
2017-12-13 10:47:57 +0300

    Доработан механизм экспорта в CSV

--- FC/Class/Utils/CSVhelper.cs
+++ FC/Class/Utils/CSVhelper.cs
@@ -34,20 +34,8 @@ namespace FilmCollection
                         catch (Exception ex) { Logs.Log("Невозможно удалить файл отчета", ex); }
                     }
 
-                    string currentFolder = fileDialog.FileName.Substring(0, fileDialog.FileName.LastIndexOf('\\'));
-
-                    string currentFolder2 = System.IO.Path.GetDirectoryName(fileDialog.FileName);
-                    FileInfo fileInfo = new FileInfo(fileDialog.FileName);
-                    string name = fileInfo.Name.Substring(0, fileInfo.Name.LastIndexOf(fileInfo.Extension));
-
-                    fileReport = fileInfo.Name.Substring(0, fileInfo.Name.LastIndexOf(fileInfo.Extension));
-
-                    string targetDir = Path.Combine(Path.GetDirectoryName(fileDialog.FileName), Getfolder());
-
-                    if (!Directory.Exists(targetDir))
-                    {
-                        Directory.CreateDirectory(targetDir);
-                    }                    
+                    //Encoding encoding = Encoding.GetEncoding("windows-1251");
+                    //string filePath = @"X:\test.csv";
 
                     List<Record> filtered = new List<Record>();
                     RC.CombineList.ForEach(r => filtered.AddRange(r.recordList));
@@ -61,31 +49,10 @@ namespace FilmCollection
                         var newLine = $"{rec.mName};{rec.FileName};{rec.FilePath};{rec.mDescription};{rec.mCountry};{rec.mGenre};{rec.mCategory};{rec.TimeString}";
                         csvdata.AppendLine(newLine);
                     }
-                    File.WriteAllText(fileReport, csvdata.ToString(), Encoding.GetEncoding(1251));
-
-                    //SaveReport(fileDialog.FileName);
+                    File.WriteAllText(fileDialog.FileName, csvdata.ToString(), Encoding.GetEncoding(1251));
                     System.Diagnostics.Process.Start(fileDialog.FileName);
                 }
             }
-
-
-            ////Encoding encoding = Encoding.GetEncoding("windows-1251");
-
-            //string filePath = @"X:\test.csv";
-
-            //List<Record> filtered = new List<Record>();
-            //RC.CombineList.ForEach(r => filtered.AddRange(r.recordList));
-
-            //StringBuilder csvdata = new StringBuilder();
-            //string head = "Название;Название файла;Полный путь;Описание;Страна;Жанр;Категория;Продолжительность";
-            //csvdata.AppendLine(head);
-
-            //foreach (Record rec in filtered)
-            //{
-            //    var newLine = $"{rec.mName};{rec.FileName};{rec.FilePath};{rec.mDescription};{rec.mCountry};{rec.mGenre};{rec.mCategory};{rec.TimeString}";
-            //    csvdata.AppendLine(newLine);                  
-            //}
-            //File.WriteAllText(filePath, csvdata.ToString(), Encoding.GetEncoding(1251));
         }
     }
 }
