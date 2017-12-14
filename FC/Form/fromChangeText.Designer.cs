60d42fd770c3dae7a7d22d37a3642d31d5c3709e
hmaster20 <hmaster20@gmail.com>
2017-12-14 22:53:53 +0300

    добавлена форма правки текстовой строки

---
+++ FC/Form/fromChangeText.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace FilmCollection
+{
+    partial class fromChangeText
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.tbChangeText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnOk
+            // 
+            this.btnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOk.Location = new System.Drawing.Point(124, 119);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 0;
+            this.btnOk.Text = "Ok";
+            this.btnOk.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(263, 119);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 0;
+            this.btnCancel.Text = "Отмена";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // tbChangeText
+            // 
+            this.tbChangeText.Location = new System.Drawing.Point(30, 50);
+            this.tbChangeText.Name = "tbChangeText";
+            this.tbChangeText.Size = new System.Drawing.Size(387, 20);
+            this.tbChangeText.TabIndex = 1;
+            // 
+            // fromChangeText
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 170);
+            this.Controls.Add(this.tbChangeText);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOk);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "fromChangeText";
+            this.ShowIcon = false;
+            this.Text = "Укажите новое значение";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancel;
+        internal System.Windows.Forms.TextBox tbChangeText;
+    }
+}
---
+++ FC/Form/fromChangeText.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FilmCollection
+{
+    public partial class fromChangeText : Form
+    {
+        public fromChangeText()
+        {
+            InitializeComponent();
+        }
+
+        internal void ShowDialog(string textForChange)
+        {
+            InitializeComponent();            
+        }
+    }
+}
