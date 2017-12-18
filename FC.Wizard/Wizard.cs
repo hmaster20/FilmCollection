889fcf89ce3980652c0118309c6fda275a4417c2
hmaster20 <hmaster20@gmail.com>
2017-12-18 22:48:39 +0300

    Проект мастера

---
+++ FC.Wizard/Wizard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FC.Wizard
+{
+    public partial class Wizard : Form
+    {
+        public Wizard()
+        {
+            InitializeComponent();
+        }
+    }
+}
