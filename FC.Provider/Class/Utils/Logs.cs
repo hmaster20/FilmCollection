a70e7c41a97a9fdfe5705808bee5bbc4ebb26f20
hmaster20 <hmaster20@gmail.com>
2023-04-04 23:48:01 +0300

    Рефакторинг класса журналирования

--- FC.Provider/Class/Utils/Logs.cs
+++ FC.Provider/Class/Utils/Logs.cs
@@ -8,29 +8,31 @@ using System.Windows.Forms;
 
 namespace FC.Provider
 {
+    /// <summary>Класс для форматирования ошибок и соохраннеия в текстовый файл</summary>
     public static class Logs
     {
+        /// <summary>Указываем имя лог файла</summary>
+        private static string logFile = "log.txt";
+
         public static void Log(string message, Exception ex)
         {
             if (ex != null)
             {
+                // Почитать habrahabr.ru/post/98638/
                 MessageBox.Show(ex.Message);
 
-                File.AppendAllText("log.txt", "\r\n-------------------");
-                File.AppendAllText("log.txt", (string.Format($"\r\n{DateTime.Now.ToString("yyyy.MM.dd_HH:mm:ss")}\r\n")));
-                File.AppendAllText("log.txt", message);
-                File.AppendAllText("log.txt", "\r\n");
-                //File.AppendAllText("log.txt", ex.Message);
-                File.AppendAllText("log.txt", ex.ToString());
-
-                // Почитать habrahabr.ru/post/98638/
+                File.AppendAllText(logFile, "\r\n-------------------");
+                File.AppendAllText(logFile, (string.Format($"\r\n{DateTime.Now.ToString("yyyy.MM.dd_HH:mm:ss")}\r\n")));
+                File.AppendAllText(logFile, message);
+                File.AppendAllText(logFile, "\r\n");
+                File.AppendAllText(logFile, ex.ToString());
             }
             else
             {
-                File.AppendAllText("log.txt", "\r\n-------------------");
-                File.AppendAllText("log.txt", (string.Format($"\r\n{DateTime.Now.ToString("yyyy.MM.dd_HH:mm:ss")}\r\n")));
-                File.AppendAllText("log.txt", message);
-                File.AppendAllText("log.txt", "\r\n");
+                File.AppendAllText(logFile, "\r\n-------------------");
+                File.AppendAllText(logFile, (string.Format($"\r\n{DateTime.Now.ToString("yyyy.MM.dd_HH:mm:ss")}\r\n")));
+                File.AppendAllText(logFile, message);
+                File.AppendAllText(logFile, "\r\n");
             }
         }
     }
