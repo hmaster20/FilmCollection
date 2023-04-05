1b42ce81eeeaadbb368e8a7c2180f8a5593aaa74
hmaster20 <hmaster20@gmail.com>
2023-04-05 16:31:08 +0300

    refactoring

--- FC.Provider/Generic.cs
+++ FC.Provider/Generic.cs
@@ -9,33 +9,21 @@ namespace FC.Provider
     /// <summary>Класс формирующий информацию о файле базы</summary>
     public static class Generic
     {
-        ///// <summary>Файл базы коллекции "VideoList.xml"</summary>
-        ////[XmlIgnore]
-        ////public static string BaseName { get; } = "VideoList.xml";
-        ////static readonly string BaseName = "VideoList.xml";
-        //public static string BaseName = "VideoList.xml";
-
         /// <summary>Файл базы коллекции по умолчанию "VideoList.xml"</summary>
         private static string baseName = "VideoList.xml";
-
-        //private static string baseName = "Unknown";
 
         public static void SetBaseName(string name)
         {
             //if (string.IsNullOrWhiteSpace(name))
-            //{
             //    baseName = "Unknown";
-            //}
             //else
-            //{
             //    baseName = name;
-            //}
+
             if (!string.IsNullOrWhiteSpace(name))
             {
                 baseName = name;
             }
         }
-
 
         /// <summary>Метод извлекает имя базы</summary>
         public static string GetBaseName()
